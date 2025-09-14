7ff75ceb52c9b85ad91b51e91ef353534af4f16b
bitsandpixel <vishnukiran90@gmail.com>
2025-09-14 16:42:58 +0530

    added sample data of users to check json output, changed previous users dataset to ids, sending users arraay as json output in exp.js, added views folder - added index file in it downloaded ejs(embedded Javascript) module to project to generate dynamic HTML markup using javascript code within HTML template added ejs dependency in package.json manifest for the project

--- exp.js
+++ exp.js
@@ -1,36 +1,61 @@
 const express = require('express');
-const exp = express();
+const app = express();
 const PORT = 9000;
 
 /**
  * Creating a simple data source
  */
-const users = {
+const ids = {
   20: 'Welcome X',
   21: 'Welcome Y',
   22: 'Welcome Z',
 };
 
-/* exp.get('/', function (req, res) {
+// Sample user data
+const users = [
+    { id: 1, name: 'Alice', email: 'alice@example.com' },
+    { id: 2, name: 'Bob', email: 'bob@example.com' },
+    { id: 3, name: 'Charlie', email: 'charlie@example.com' }
+];
+
+// Route to get all users
+app.get('/users', (req, res) => {
+    res.json(users); // Sends the 'users' array as JSON
+});
+
+// Route to get a single user by ID
+app.get('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const user = users.find(u => u.id === userId);
+
+    if (user) {
+        res.json(user); // Sends the 'user' object as JSON
+    } else {
+        res.status(404).json({ message: 'User not found' }); // Sends an error message as JSON with a 404 status
+    }
+});
+
+
+/* app.get('/', function (req, res) {
   //   console.log('Hello World');
   res.send('Hello World');
 });
 
-exp.get('/welcome', function (req, res) {
+app.get('/welcome', function (req, res) {
   res.send('Welcome back guys');
 }); */
 
 //how to accept a dynamic URL
 // Defining a dynamic route using a route parameter ':id'
-//exp.get('/welcome/:id', function (req, res) {
-exp.get('/welcome', function (req, res) {
+//app.get('/welcome/:id', function (req, res) {
+app.get('/welcome', function (req, res) {
   //getting id from the route(URL) paramters
   //   const id = req.params.id; //for using route params
   const id = req.query.id; //for query params
   //checking if the id exists in the data source
-  if (users[id]) {
+  if (ids[id]) {
     //if it exists
-    res.send(users[id] + ' id:' + id);
+    res.send(ids[id] + ' id:' + id);
   } else {
     //if it does not exists in the data source.
     res.status(404).send('Error: ID not found. Please provide a proper ID');
@@ -40,6 +65,6 @@ exp.get('/welcome', function (req, res) {
 /**
  * 
  */
-exp.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
 });
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const express = require('express')
  */
 const app = express()
 const PORT = 3000;
+app.set('view engine', 'ejs')
 
 /**
  * 
@@ -17,15 +18,33 @@ const PORT = 3000;
 app.get('/', (req,res)=>{
     console.log('Here')
     // res.send('Hi')
-    res.sendStatus(500)
     // res.sendStatus(500)
+    // res.status(500).send('<user defined message>')
+    
+    /**
+     * default status code is 200, so we do not need to include it.
+     */
+    //res.status(200).json({message:"Successful"})
+    // res.json({message:"Successful"})
+
+    /**
+     * If you have to give user the permission to download.
+     * you can use
+     */
+    // res.download("README.md")
+res.render('index')
+    /**
+     * 
+     */
+
+
+
 })
 
 app.get('/welcome', (req,res)=>{
     console.log("Entered /welcome folder")
-    res.sendStatus(200)
+    res.json({ message: 'Welcome to the folder' })
 })
-
 /**
  * our server is listening on given port-number
  */
